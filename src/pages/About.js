367b9dcca15028101e1bc4e94e745dea5b8c0c26
Aqua55202 <aqua55202@gmail.com>
2022-01-07 13:46:33 +0630

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <section className="section about-section">
+            <h1 className="section-title">about us</h1>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id
+                vitae earum nisi ipsam eligendi! Accusantium est ipsa ad
+                possimus. Quisquam suscipit tempora repellendus facere accusamus
+                consectetur eos provident, delectus vitae, quo adipisci nihil
+                esse? Fugit in quia ipsum asperiores quibusdam!
+            </p>
+        </section>
+    );
+};
+
+export default About;
